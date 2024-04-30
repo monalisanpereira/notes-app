2e54313c6236f1878ee34ddd67bdb8c1e24320c2
monalisanpereira <monalisanpereira@gmail.com>
2024-04-30 18:51:50 +0900

    add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Note } from './models/note';
+
+function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    async function loadNotes() {
+      try {
+        const response = await fetch("/api/notes", { method: "GET"});
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return (
+    <div className="App">
+      {JSON.stringify(notes)}
+    </div>
+  );
+}
+
+export default App;
