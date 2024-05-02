dac4ddfb0bcc19f48ac9f1971b75446f6bea1120
monalisanpereira <monalisanpereira@gmail.com>
2024-05-02 23:56:10 +0900

    add basic styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Note } from './models/note';
+import { Note as NoteModel } from './models/note';
+import Note from './components/note';
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<NoteModel[]>([]);
 
   useEffect(() => {
     async function loadNotes() {
@@ -21,8 +21,10 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {JSON.stringify(notes)}
+    <div>
+      {notes.map(note => (
+        <Note note={note} key={note._id} />
+      ))}
     </div>
   );
 }
